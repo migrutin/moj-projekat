e8175a5701feae9be4f2342ed64c6d424a62dfaf
milicanina <migrutinovic13@gmail.com>
2023-07-03 16:24:30 +0200

    Header pt2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react'
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'alpinejs'
+import logo from '../img/logo.png';
+
 
 
 
@@ -9,9 +11,11 @@ function Header() {
     const [dropDown, setDropDown] = useState(false)
 
     return (
-        <header className="bg-blue-950 py-4 text-base">
-            <div className="text-base flex justify-between items-center px-4 mx-32">
-                <div className="flex h-10">
+       
+        <header className="bg-blue-950 py-2 text-base">
+            <div className="text-base flex justify-between items-center px-4 mx-44">
+             {/* prvi red u Headeru */}
+                <div className="flex h-10 mb-0">
 
                     <div onMouseOver={() => setDropDown(true)} onMouseLeave={() => setDropDown(false)}
                         className="flexitems-center space-x-2 cursor-pointer mr-3">
@@ -45,7 +49,7 @@ function Header() {
                 </div>
 
 
-                <div className="flex h-10  pb-5">
+                <div className="flex h-8 pb-5">
 
                     <div className="flex items-center space-x-2 cursor-pointer mr-3 mt-1">
                         <i className="fas fa-box text-white"></i>
@@ -63,11 +67,28 @@ function Header() {
 
 
 
+                            {/* Drugi red u Headeru */}
+            <div className='flex mt-0'>
+                <div className=" flex justify-between items-center h-24 px-4 ml-44 mr-5 my-0">
+                    <img src={logo} alt="Logo" className='h-28 w-40 my-2' />
+                </div>
 
+                <input type="text" id="search" placeholder="Unesite pojam za pretragu"
+                    className="ml-0 h-10 w-1/2 mt-7 px-4 py-1 border border-gray-300 rounded-full"></input>
 
-            <div className="bg-pink-200 flex justify-between items-center px-4 mx-32 my-5">
+                <div className="flex items-center space-x-2 cursor-pointer mr-3 mt-1 ml-4">
+                    <i className="fa-regular fa-user text-white"></i>
+                    <span className='text-white'>Prijava</span>
+                </div>
+
+                <div className="flex items-center space-x-2 cursor-pointer mr-3 mt-1 ml-4">
+                    <i className="fa fa-shopping-cart text-white"></i>
+                    <span className='text-white'>Korpa</span>
+                </div>
+
 
             </div>
+
 
         </header>
     )
