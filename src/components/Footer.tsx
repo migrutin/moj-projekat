98724690340592cb3965b39cd7fa4d014ea425e8
milicanina <migrutinovic13@gmail.com>
2023-07-04 18:41:06 +0200

    promena linkova

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import visa from '../img/visa.png'
 import master from '../img/master.png'
 import maestro from '../img/maestro.png'
@@ -16,40 +17,63 @@ function Footer() {
     return (
         <div className='bg-blue-950 flex-col h-28 w-full p-5 justify-center'>
             <div className='flex space-x-1 justify-between px-72 mb-5'>
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={visa} alt="" className='h-10 w-10' />
-                </div>
+                <Link to='https://rs.visa.com/'>
+                    <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                        <img src={visa} alt="" className='h-10 w-10 cursor-pointer' />
+                    </div>
+                </Link>
 
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={master} alt="" className='h-8 w-10' />
-                </div>
+                <Link to='https://www.mastercard.rs/sr-rs.html'>
+                    <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                        <img src={master} alt="" className='h-8 w-10 cursor-pointer' />
+                    </div>
+                </Link>
 
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={maestro} alt="" className='h-8 w-10' />
-                </div>
+                <Link to='https://www.mastercard.rs/sr-rs/korisnici/pronadite-karticu.html'>
+                    <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                        <img src={maestro} alt="" className='h-8 w-10 cursor-pointer' />
+                    </div>
+                </Link>
 
+                <Link to='https://rs.visa.com/pay-with-visa/security-and-assistance/protected-everywhere.html'>
                 <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={verified} alt="" className='h-10 w-10' />
+                    <img src={verified} alt="" className='h-10 w-10 cursor-pointer' />
                 </div>
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={intesa} alt="" className='h-10 w-10' />
+                </Link>`
+               
+                <Link to='https://www.bancaintesa.rs/'>
+                    <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                        <img src={intesa} alt="" className='h-10 w-10 cursor-pointer' />
+                    </div>
+                </Link>
+
+                <Link to='https://www.nlbkb.rs/'><div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                    <img src={nlb} alt="" className='h-10 w-16 cursor-pointer' />
                 </div>
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={nlb} alt="" className='h-10 w-16' />
+                </Link>
+
+                <Link to='https://www.raiffeisenbank.rs/'> <div className='bg-white h-8 w-20 rounded-lg flex items-center justify-center'>
+                    <img src={raiffeisen} alt="" className='h-10 w-16 cursor-pointer' />
                 </div>
-                <div className='bg-white h-8 w-20 rounded-lg flex items-center justify-center'>
-                    <img src={raiffeisen} alt="" className='h-10 w-16' />
+                </Link>
+            
+                <Link to='https://www.wspay.rs/'>
+                    <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                        <img src={wspay} alt="" className='h-8 w-12 cursor-pointer' />
+                    </div>
+                </Link>
+
+                <Link to='https://rs.visa.com/'>
+                    <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                        <img src={visasecure} alt="" className='h-8 w-10 cursor-pointer' />
+                    </div>
+                </Link>
+
+                <Link to='https://www.mastercard.rs/sr-rs/korisnici/podrska/sigurnost-i-zastita/identity-check.html'> <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
+                    <img src={idcheck} alt="" className='h-10 w-10 cursor-pointer' />
                 </div>
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={wspay} alt="" className='h-8 w-12' />
-                </div>
-                
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={visasecure} alt="" className='h-8 w-10' />
-                </div>
-                <div className='bg-white h-8 w-16 rounded-lg flex items-center justify-center'>
-                    <img src={idcheck} alt="" className='h-10 w-10' />
-                </div>
+                </Link>
+
 
 
             </div>
