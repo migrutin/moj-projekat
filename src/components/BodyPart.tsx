0799de1a48b7f88020424755c5ec4975d8a7b88a
milicanina <migrutinovic13@gmail.com>
2023-07-04 12:17:14 +0200

    Najtrazeniji proizvodi

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BodyPart() {
     return (
-        <div className='bg-gray-100 max-w-full h-screen'>
+        <div className='bg-gray-100 max-w-full h-full'>
             <div className='flex bg-transparent h-96 w-3/4 ml-44 space-x-6 pt-10    '>
                 <div className="relative bg-green-200 w-3/4 rounded-lg z-10">
                     <button className="absolute bottom-2 right-2 rounded-full
@@ -40,60 +40,78 @@ function BodyPart() {
             </div>
 
 
-
+        {/* Akicije, popusti, isporuka - section */}
 
             <div className='bg-white rounded-lg h-28 w-3/4 mt-10 ml-44 
             pt-auto flex  justify-between px-10 py-2'>
 
 
                 <div className="h-24 w-1/4 justify-between flex items-center">
-                <i className="fas fa-tag fa-2xl"></i>
-                        <div className="flex flex-col ml-4">
-                            <h2 className="font-semibold">Najnovije akcije</h2>
-                            <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
-                        </div>
+                    <i className="fas fa-tag fa-2xl"></i>
+                    <div className="flex flex-col ml-4">
+                        <h2 className="font-semibold">Najnovije akcije</h2>
+                        <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
+                    </div>
                     <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
-                  
+
                 </div>
 
                 <div className=" h-24 w-1/4 justify-between flex items-center ">
-                <i className="fa-solid fa-database fa-2xl"></i>
-                        <div className="flex flex-col ml-4">
-                            <h2 className="font-semibold">Osvojite popuste</h2>
+                    <i className="fa-solid fa-database fa-2xl"></i>
+                    <div className="flex flex-col ml-4">
+                        <h2 className="font-semibold">Osvojite popuste</h2>
                         <a href="https://gigatron.rs/akcije" className="underline  ml-2">Saznajte više</a>
                     </div>
                     <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
-                  
+
                 </div>
 
                 <div className="h-24 w-1/4 justify-between flex items-center ">
-                <i className="fa-solid fa-money-check-dollar fa-2xl"></i>
-                        <div className="flex flex-col ml-4 ">
-                            <h2 className="font-semibold ">Kupovina na rate</h2>
-                            <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
-                        </div>
+                    <i className="fa-solid fa-money-check-dollar fa-2xl"></i>
+                    <div className="flex flex-col ml-4 ">
+                        <h2 className="font-semibold ">Kupovina na rate</h2>
+                        <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
+                    </div>
                     <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
-                  
+
                 </div>
 
                 <div className="h-24 w-1/4 justify-between flex items-center  ">
-                <i className="fa-solid fa-truck fa-2xl"></i>
-                        <div className="flex flex-col mr-2">
-                            <h2 className="font-semibold ml-2">Isporuka</h2>
+                    <i className="fa-solid fa-truck fa-2xl"></i>
+                    <div className="flex flex-col mr-2">
+                        <h2 className="font-semibold ml-2">Isporuka</h2>
                         <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
                     </div>
                     <div className="w-0.5 h-1/2 bg-transparent mx-4 mt-2"></div>
-                   
-                  
+
+
                 </div>
 
 
 
 
-               
-
-               
             </div>
+
+
+                {/* Najtrazeniji proizvodi */}
+            <div className='bg-transparent rounded-lg h-56 w-3/4 mt-10 ml-44 
+            pt-auto flex  justify-between flex-col '>
+
+                <h1 className='font-bold text-2xl pb-5'>Najtraženiji proizvodi</h1>
+                <div className="container bg-white rounded-lg h-52 w-full"></div>
+
+            </div>
+        
+
+
+
+
+
+
+
+
+
+
 
 
 
