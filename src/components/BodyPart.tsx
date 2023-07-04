d44acf6e7454c49d3cefc67c75165cb3fe36b67f
milicanina <migrutinovic13@gmail.com>
2023-07-04 11:57:30 +0200

    Akcije, isporuka, rate, popusti - section

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -46,42 +46,43 @@ function BodyPart() {
             pt-auto flex  justify-between px-10 py-2'>
 
 
-                <div className="h-24 w-3/16 justify-between flex items-center">
-                <i className="fas fa-database fa-2xl"></i>
+                <div className="h-24 w-1/4 justify-between flex items-center">
+                <i className="fas fa-tag fa-2xl"></i>
+                        <div className="flex flex-col ml-4">
+                            <h2 className="font-semibold">Najnovije akcije</h2>
+                            <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
+                        </div>
+                    <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
+                  
+                </div>
+
+                <div className=" h-24 w-1/4 justify-between flex items-center ">
+                <i className="fa-solid fa-database fa-2xl"></i>
                         <div className="flex flex-col ml-4">
                             <h2 className="font-semibold">Osvojite popuste</h2>
-                            <a href="https://gigatron.rs/akcije" className="underline">Saznajte više</a>
-                        </div>
-                    <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
-                  
-                </div>
-
-                <div className="h-24 w-3/16 justify-between flex items-center ">
-                <i className="fa-solid fa-money-check fa-2xl"></i>
-                        <div className="flex flex-col ml-4">
-                            <h2 className="font-semibold">Kupovina na rate</h2>
-                        <a href="https://gigatron.rs/akcije" className="underline">Saznajte više</a>
+                        <a href="https://gigatron.rs/akcije" className="underline  ml-2">Saznajte više</a>
                     </div>
                     <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
                   
                 </div>
 
-                <div className="h-24 w-3/16 justify-between flex items-center ">
-                <i className="fa-solid fa-truck fa-2xl"></i>
-                        <div className="flex flex-col ml-4">
-                            <h2 className="font-semibold">Isporuka</h2>
-                            <a href="https://gigatron.rs/akcije" className="underline">Saznajte više</a>
+                <div className="h-24 w-1/4 justify-between flex items-center ">
+                <i className="fa-solid fa-money-check-dollar fa-2xl"></i>
+                        <div className="flex flex-col ml-4 ">
+                            <h2 className="font-semibold ">Kupovina na rate</h2>
+                            <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
                         </div>
                     <div className="w-0.5 h-1/2 bg-gray-200 mx-4 mt-2"></div>
                   
                 </div>
 
-                <div className="h-24 w-3/16 justify-between flex items-center  ">
+                <div className="h-24 w-1/4 justify-between flex items-center  ">
                 <i className="fa-solid fa-truck fa-2xl"></i>
-                        <div className="flex flex-col ml-4">
-                            <h2 className="font-semibold">Isporuka</h2>
-                        <a href="https://gigatron.rs/akcije" className="underline">Saznajte više</a>
+                        <div className="flex flex-col mr-2">
+                            <h2 className="font-semibold ml-2">Isporuka</h2>
+                        <a href="https://gigatron.rs/akcije" className="underline ml-2">Saznajte više</a>
                     </div>
+                    <div className="w-0.5 h-1/2 bg-transparent mx-4 mt-2"></div>
                    
                   
                 </div>
