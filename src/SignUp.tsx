8e38428d014e595dccb88b8cb559105159e572f6
milicanina <migrutinovic13@gmail.com>
2023-07-05 18:31:23 +0200

    signup promena naziva

---
+++ src/SignUp.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+import Underline from './components/Underline'
+import Newsletter from './components/Newsletter'
+import Footer from './components/Footer'
+
+function SignUp() {
+  return (
+    <div>
+        <Header></Header>
+        <Underline></Underline>
+        <SignUp></SignUp>
+        <Newsletter></Newsletter>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default SignUp
