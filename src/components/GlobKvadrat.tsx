0d7314511f5f0131bc98b3b9414031f443274358
milicanina <migrutinovic13@gmail.com>
2023-07-07 23:51:25 +0200

    nesto

---
+++ src/components/GlobKvadrat.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import vivo from '../img/vivo.png'
+import tarifa from '../img/tarife.png'
+
+function GlobKvadrat() {
+  return (
+    <div className="globaltel-kvadrat">
+                    <h1 className='telefon'>VIVO Y22s 6/128GB Startlit Blue </h1>
+                    <div className='flex space-x-3 mt-5'>
+                        <img src={vivo} alt="" className='h-72 w-64 mt-10' />
+                        <img src={tarifa} alt="" className='h-72 w-56 mt-10'/>
+                    </div>
+                    <button className='globaltel-dugme'>Pogledajte ponudu</button>
+                </div>
+  )
+}
+
+export default GlobKvadrat
---
+++ src/components/GlobTekst.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function GlobTekst() {
+  return (
+    <div>
+        <p className='globaltel-p'> <span className='font-bold'>Gigatron i Globaltel</span> čine fantastičnu kombinaciju!
+                    U Gigatron prodavnicama vas očekuje mogućnost da pri kupovini novog uređaja izaberete i neku od Globaltel tarifa, čime ostvarujete posebne cene na odabrane uređaje.
+                    Ponuda se može ostvariti isključivo u Gigatron prodajnim objektima.</p>
+                <h1 className='globaltel-h'>Super cena uređaja uz svaki Globaltel postpaid paket</h1>
+
+    </div>
+  )
+}
+
+export default GlobTekst
