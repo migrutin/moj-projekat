f3a3e15650b01295feecbb5d10ad484b4be6c5e5
milicanina <migrutinovic13@gmail.com>
2023-07-12 23:27:06 +0200

    izgled

--- src/context/CartItem.tsx
+++ src/context/CartItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useShoppingCart } from './ShoppingCartContext'
 import laptopsJson from '../data2/laptops.json'
+import { Link } from 'react-router-dom'
 
 type CartItemProps={
     id:number
@@ -13,9 +14,28 @@ export default function CartItem({id, quantity}: CartItemProps) {
     if(item== null) return null
 
     return (
-        <div>
+        <div className='flex bg-gray-100'>
             <div className='cart'>
                 <img src={item.imgUrl} alt="" className='w-56 h-56' />
+            </div>
+
+            <div className='shopping-cart-text'>
+                <h1 className='cart-name'>{item.name} {quantity > 1 && <div className='text-gray-700 my-2'>x{quantity}</div>}</h1>
+                <p className='cart-price'>{item.price}</p>
+               <div></div>
+            </div>
+
+            <button className='remove'>x</button>
+
+            <div className='div-cart-2'>
+                <div className='flex flex-ol'>
+                    <h1>Iznos kupovine je: </h1> 
+                    <div className='flex total'>{item.price*quantity}</div></div>
+                <div className='cart-link'>
+                    <Link to='/registracija' className='underline'>Napravite novi nalog ili se ulogujte</Link>
+                </div>
+
+                <button className='cart-button'>Nastavite</button>
             </div>
         </div>
     )
