358761f01428d62b06a59f173428881482a84103
milicanina <migrutinovic13@gmail.com>
2023-07-15 15:03:49 +0200

    oneLaptop-removed green bg

--- src/components/OneLaptop.tsx
+++ src/components/OneLaptop.tsx
@@ -18,7 +18,7 @@ const OneLaptop=(oneLaptop:OneLaptopProps)=>{
     <Link to='/laptops' className='link' key={oneLaptop.id}>
     <img src={oneLaptop.imgUrl} alt="aaaa" className='laptop-item-img' />
     <h1 className='link-h'>{oneLaptop.name}</h1>
-    <div className='flex bg-green-200 justify-between'>
+    <div className='flex justify-between'>
       <p className='link-p'>{oneLaptop.price} RSD</p>
       
       <i className="fa fa-cart-shopping fa-2xl mt-5 mr-2"  onClick={() =>increaseQuantity(oneLaptop)}></i> 
