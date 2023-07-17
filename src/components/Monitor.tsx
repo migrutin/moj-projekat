146e37ac2298f7f0831773832a9ec40646391902
milicanina <migrutinovic13@gmail.com>
2023-07-17 21:59:24 +0200

    male prepravke - finalni ProductDropdown

--- src/components/Monitor.tsx
+++ src/components/Monitor.tsx
@@ -15,7 +15,7 @@ const Monitor = ({ arrayMonitor }: TypeMonitorProps) => {
   return (
     <div className='laptop-items'>
       <div className='laptop-div-1'>
-        <h1 className='laptop-h'>Brand name računari</h1>
+        <h1 className='laptop-h'>Monitori</h1>
       </div>
       <div className='laptop'>
         <div className='laptop-item'>
