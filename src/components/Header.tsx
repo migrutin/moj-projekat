e9061ca0da72050a2952ff4d5567f41e0ba6b248
milicanina <migrutinovic13@gmail.com>
2023-07-19 17:33:50 +0200

    header full

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
     return (
 
         <header className='header flex top-0'>
-            <div className="header-1 lg:visible py-2">
+            <div className="header-1 w-1/3 lg:visible py-2">
                 <div onMouseOver={() => setDropDown(true)} onMouseLeave={() => setDropDown(false)}
                     className="cursor-pointer">
                     <div className='space-x-2'>
@@ -57,16 +57,16 @@ const Header = () => {
 
 
             </div>
-            <div className="flex flex-col items-end w-1/3  py-2 px-2 space-y-2">
+            <div className="flex justify-end mt-5 space-x-3 w-1/3 ">
                 
                 <Link to='/login' ><i className="fa-regular fa-user text-white fa-xl"></i></Link>
 
-                <Link to='/korpa'> <i className="fa fa-shopping-cart text-white fa-xl"></i></Link>
-                        {/* {totalNumberItems >  0 && (
-                             <div className='rounded-full bg-yellow-400
-                        h-5 w-5 flex justify-center align-middle absolute -mt-7 -ml-3'>
-                            {totalNumberItems}
-                        </div>)} */}
+                <Link to='/korpa'> <i className="fa fa-shopping-cart text-white  fa-xl">
+                        
+                        {totalNumberItems > 0 && (<div className='yellow-circle flex -mt-7 -ml-3'> {totalNumberItems} </div>)}
+                    
+                    </i>
+                    </Link>
                        
                 
             </div>
